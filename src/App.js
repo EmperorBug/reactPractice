1898638a8b9bf6d5ae63de777e476d32d3827cef
kim <akdl4613@naver.com>
2025-10-31 15:31:31 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Title from './components/title'
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Title title='TODO LIST'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/todoService.js
@@ -0,0 +1,15 @@
+export function createTodo(todos, newTodo) {
+    return [...todos, newTodo];
+}
+
+export function updateTodo(todos, idx, newText) {
+    return todos.map((t) => {
+        t.idx == idx ? {...t, text:newText} : t
+    });
+}
+
+export function delTodo(todos, idx) {
+    return todos.filter((t) => {
+        t.idx !== idx
+    })
+}
